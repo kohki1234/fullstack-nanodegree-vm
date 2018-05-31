b7a99579577a1fcc991c72670fa7934ebe53e1f8
Kohki Sato <you@example.com>
2018-05-31 12:39:26 +0900

    Seems like I messed up...

--- vagrant/finalproject.py
+++ vagrant/finalproject.py
@@ -16,12 +16,9 @@ session = DBSession()
 def showRestaurants():
 	restaurants = session.query(Restaurant).all()
 
-	for n in restaurants:
-		restaurant_id = n.id
-		restaurant_name = n.name
-		#print restaurant_name
 
-	return render_template('restaurants.html',restaurants=restaurants, restaurant_id=restaurant_id)
+
+	return render_template('restaurants.html',restaurants=restaurants)
 
 
 @app.route("/restaurant/new/", methods=['GET','POST'])
@@ -35,10 +32,11 @@ def newRestaurant():
 	else:
 		return render_template('newrestaurant.html', restaurant=restaurant, restaurant_id=restaurant_id)
 
-@app.route("/restaurant/<int:restaurant_id>/edit")
+@app.route("/restaurant/<int:restaurant_id>/edit", methods=['GET','POST'])
 
 def editRestaurant(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+
 
 	return render_template('editrestaurant.html', restaurant_id=restaurant_id, restaurant=restaurant)
 
